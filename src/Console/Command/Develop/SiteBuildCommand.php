2977e93f3d9a9179063caaea6652e849ace3e46b
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-11-10 12:05:33 +0000

    Make commands visible when you use drupal list

--- src/Console/Command/Develop/SiteBuildCommand.php
+++ src/Console/Command/Develop/SiteBuildCommand.php
@@ -7,6 +7,10 @@
 
 namespace VM\Console\Command\Develop;
 
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 /**
  * Class SiteBuildCommand
  *
