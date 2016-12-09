b4536e634065a5f2c61f4addcf27e528e3b27c24
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-12-09 00:33:53 +0000

    Missing class

--- src/Command/SiteBaseCommand.php
+++ src/Command/SiteBaseCommand.php
@@ -9,6 +9,8 @@
 
 namespace DennisDigital\Drupal\Console\Command;
 
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command;
