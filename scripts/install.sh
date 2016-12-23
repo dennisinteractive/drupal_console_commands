b34924e61dacce418167234f429a59a157af0f73
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-12-23 21:56:49 +0000

    Added bash script to perform installetion

---
+++ scripts/install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Performs an installation of Drupal console commands
+set -x
+
+BIN_FOLDER="/usr/local/bin"
+BIN_FILE="${BIN_FOLDER}/drupal"
+
+# Delete existing bin file
+if [ -e "${BIN_FILE}" ]; then
+  sudo rm ${BIN_FILE}
+fi
+
+# Download latest bin
+curl https://drupalconsole.com/installer -L -o ${BIN_FILE}
+chmod +x ${BIN_FILE}
+
+drupal init --override
+
+# Install console extend
+# rm -rf ~/.composer/extend;
+composer create-project drupal/console-extend:dev-master#227eb6e1d0d8d2abee09dde4e9a1044f301c93e1 ~/.console/extend
+
+# Install custom commands
+cd ~/.console/extend; composer require dennisdigital/drupal_console_commands:dev-drupal_extend --update-no-dev
+
+composer update
+
+
