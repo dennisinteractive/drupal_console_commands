c9e2ef5fcf9cd1341dd143f007bf43d6b321cfd3
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-01-27 11:41:20 +0000

    Fixed parameters

--- scripts/site_build.sh
+++ scripts/site_build.sh
@@ -21,12 +21,12 @@ if [ "$2" = "" ]
 fi
 
 drupal site:compose ${SITENAME} && \
-drupal site:npm --placeholder"name:${SITENAME}" && \
-drupal site:grunt --placeholder"name:${SITENAME}" && \
+drupal site:npm --placeholder="name:${SITENAME}" && \
+drupal site:grunt --placeholder="name:${SITENAME}" && \
 drupal site:settings:db ${SITENAME} && \
 drupal site:phpunit:setup ${SITENAME} && \
 drupal site:behat:setup ${SITENAME} && \
 drupal site:settings:memcache ${SITENAME} && \
 drupal site:db:import ${SITENAME} && \
-cd /vagrant/repos/%{{name}}/web; drush updb -y
-cd /vagrant/repos/%{{name}}/web; drush cr;
+cd /vagrant/repos/${SITENAME}/web; drush updb -y
+cd /vagrant/repos/${SITENAME}/web; drush cr;
