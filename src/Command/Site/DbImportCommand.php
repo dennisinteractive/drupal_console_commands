2a555a82954b2f84ab56f9b0ef2d2df72411021c
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-07-17 14:26:41 +0100

    Use specific commands depending on drupal core

--- src/Command/Site/DbImportCommand.php
+++ src/Command/Site/DbImportCommand.php
@@ -147,35 +147,43 @@ class DbImportCommand extends AbstractCommand {
     }
 
     // If a dump file wasn't found or not specified, do a fresh site install
+    $commands = [];
     if (empty($this->filename) || !$this->fileExists($this->filename)) {
       //@todo Use drupal site:install instead of Drush.
-      $command = sprintf(
-        'cd %s && ' .
-        'chmod 777 ../default && ' .
-        'chmod 777 settings.php && ' .
-        'drush si -y %s %s && ' .
-        'drush cset "system.site" uuid "$(drush cget system.site uuid --source=sync --format=list)" -y',
-        $this->shellPath($this->getSiteRoot()),
-        $this->profile,
-        $options
-      );
       $this->io->comment('Installing site');
+
+      // Fix site folder permissions.
+      if ($this->hasSiteRoot()) {
+        $commands[] = sprintf('chmod 777 %s',  $this->shellPath($this->getSiteRoot()));
+        $commands[] = sprintf('chmod 777 %ssettings.php', $this->shellPath($this->getSiteRoot()));
+      }
+
+      // Site install.
+      $commands[] = sprintf('cd %s', $this->shellPath($this->getSiteRoot()));
+      $commands[] = sprintf('drush si -y %s %s', $this->profile, $options);
+
+      // Drupal 8 only;
+      if ($this->drupalVersion === 8) {
+        // Set uuid from config.
+        $commands[] = sprintf('drush cset "system.site" uuid "$(drush cget system.site uuid --source=sync --format=list)" -y');
+      }
     }
     else {
+      $this->io->comment('Importing dump');
+
       if (is_null($input->getOption('db-name'))) {
         $input->setOption('db-name', $this->siteName);
       }
-      $command = sprintf(
-        'cd %s; ' .
-        'drush sql-create %s -y; ' .
-        'drush sql-cli < %s; ',
-        $this->getSiteRoot(),
-        $input->getOption('db-name'),
-        $this->filename
-      );
-      $this->io->comment('Importing dump');
+
+      $commands[] = sprintf('cd %s', $this->shellPath($this->getSiteRoot()));
+
+      // Create DB;
+      $commands[] = sprintf('drush sql-create %s -y', $input->getOption('db-name'));
+      // Import dump;
+      $commands[] = sprintf('drush sql-cli < %s', $this->filename);
     }
 
+    $command = implode(' && ', $commands);
     $this->io->commentBlock($command);
 
     // Run.
