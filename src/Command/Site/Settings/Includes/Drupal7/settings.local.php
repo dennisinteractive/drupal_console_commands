0a4538dcc709d13879344eb250030679a7bde8c8
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-07-18 23:12:13 +0100

    Fixed D7 settings

--- src/Command/Site/Settings/Includes/Drupal7/settings.local.php
+++ src/Command/Site/Settings/Includes/Drupal7/settings.local.php
@@ -1,6 +1,6 @@
 <?php
 // Set Stage file proxy origin.
-$conf['stage_file_proxy.settings']['origin'] = '${cdn}';
+$conf['stage_file_proxy_origin'] = '${cdn}';
 
 // Change CDN domain to local.
-$conf['cdn.settings']['mapping']['domain'] = '${host}';
+$conf['cdn_basic_mapping'] = '${host}|.css .js .jpg .jpeg .png';
--- src/Command/Site/Settings/Includes/Drupal7/settings.memcache.php
+++ src/Command/Site/Settings/Includes/Drupal7/settings.memcache.php
@@ -2,7 +2,7 @@
 /**
  * Memcache settings.
  */
-$conf['memcache']['servers'] = ['127.0.0.1:11211' => 'default'];
-$conf['memcache']['bins'] = ['default' => 'default'];
-$conf['memcache']['key_prefix'] = '${memcache_prefix}';
-$conf['memcache']['stampede_protection'] = TRUE;
+$conf['memcache_servers'] = ['127.0.0.1:11211' => 'default'];
+$conf['memcache_bins'] = ['cache' => 'default'];
+$conf['memcache_key_prefix'] = '${memcache_prefix}';
+$conf['memcache_stampede_protection'] = TRUE;
