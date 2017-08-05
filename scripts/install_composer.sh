4c845c902090625099a70e7f93df8f211933b989
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-08-05 05:21:54 +0100

    dont check

--- scripts/install_composer.sh
+++ scripts/install_composer.sh
@@ -4,25 +4,20 @@
 # Check if there is already composer and only do these if needed
 COMPOSER=$(which composer)
 
-if [ -z {$COMPOSER} ]; then
+DIRECTORY="/usr/local/bin/composer"
+COMPOSER="${DIRECTORY}/composer"
 
-    DIRECTORY="/usr/local/bin/composer"
-    COMPOSER="${DIRECTORY}/composer"
+cd ~
+PATH="${PHP_FOLDER}/:$PATH" php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+PATH="${PHP_FOLDER}/:$PATH" php composer-setup.php
 
-    cd ~
-    PATH="${PHP_FOLDER}/:$PATH" php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    PATH="${PHP_FOLDER}/:$PATH" php composer-setup.php
-
-    if [ -e "${COMPOSER}" ]; then
-      sudo rm ${COMPOSER}
-    fi
-
-    if [ ! -e "${DIRECTORY}" ]; then
-      sudo mkdir DIRECTORY
-    fi
-
-    sudo mv composer.phar ${COMPOSER}
-    sudo chmod +x ${COMPOSER}
-else
-    echo "Composer is already installed on ${COMPOSER}"
+if [ -e "${COMPOSER}" ]; then
+  sudo rm ${COMPOSER}
 fi
+
+if [ ! -e "${DIRECTORY}" ]; then
+  sudo mkdir DIRECTORY
+fi
+
+sudo mv composer.phar ${COMPOSER}
+sudo chmod +x ${COMPOSER}
