17255bac4e74190fd1efd7a8206a2a980dead16c
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-08-22 01:08:56 +0100

    Site build command

---
+++ src/Command/Site/BuildCommand.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * @file
+ * Contains \DennisDigital\Drupal\Console\Command\BuildCommand.
+ *
+ * Builds the site by calling various commands.
+ */
+
+namespace DennisDigital\Drupal\Console\Command\Site;
+
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use DennisDigital\Drupal\Console\Exception\SiteCommandException;
+
+/**
+ * Class SiteBuildCommand
+ *
+ * @package DennisDigital\Drupal\Console\Command
+ */
+class BuildCommand extends AbstractCommand {
+
+  /**
+   * Stores branch information.
+   *
+   * @var array Branch.
+   */
+  protected $branch;
+
+  /**
+   * @var ShellProcess
+   */
+  private $process;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    parent::configure();
+
+    $this->setName('site:build')
+      ->setDescription('Build a site');
+
+    // Custom options.
+    $this->addOption(
+        'branch',
+        '-B',
+        InputOption::VALUE_OPTIONAL,
+        'Specify which branch to build if different than the global branch found in sites.yml'
+      );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function interact(InputInterface $input, OutputInterface $output) {
+    parent::interact($input, $output);
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    parent::execute($input, $output);
+
+    $commands = array(
+      'drupal site:checkout %s',
+      $this->getComposerMakeCommand,
+      'drupal site:npm %s',
+      'drupal site:grunt %s',
+      'drupal site:settings %s',
+      'drupal site:phpunit:setup %s',
+      'drupal site:test:setup %s',
+      'drupal site:db:import %s',
+      'drupal site:update %s',
+    );
+
+    foreach ($commands as $commandName) {
+      $command = $this->getApplication()->find($commandName);
+
+      $parameters = $input->getArguments();
+      foreach ($input->getOptions() as $name => $value) {
+        $parameters['--' . $name] = $value;
+      }
+      $commandInput = new ArrayInput(array_filter($parameters));
+
+      $command->run($commandInput, $output);
+    }
+  }
+
+  /**
+   * Detect if the site uses composer or make files.
+   */
+  private function getComposerMakeCommand() {
+   if (file_exists($this->getSiteRoot() . '/compsoser.json')) {
+      $composerMakeCommand = 'drupal site:compose %s';
+    }
+    else if (file_exists($this->getSiteRoot() . '/site.make')) {
+      $composerMakeCommand = 'drupal site:compose %s';
+    }
+    else if (file_exists($this->getSiteRoot() . sprintf('/%s.make', $this->sitename))) {
+      $composerMakeCommand = 'drupal site:compose %s';
+    }
+    else {
+      $composerMakeCommand = '';
+    }
+
+    return $composerMakeCommand;
+  }
+
+}
