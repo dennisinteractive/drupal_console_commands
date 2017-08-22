854e34de41042bb168291a910ea847ca3b068594
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-08-22 02:03:32 +0100

    fixing command

--- src/Command/Site/BuildCommand.php
+++ src/Command/Site/BuildCommand.php
@@ -9,12 +9,14 @@
 
 namespace DennisDigital\Drupal\Console\Command\Site;
 
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use DennisDigital\Drupal\Console\Exception\SiteCommandException;
+use DennisDigital\Drupal\Console\Command\Exception\CommandException;
 
 /**
  * Class SiteBuildCommand
@@ -51,6 +53,7 @@ class BuildCommand extends AbstractCommand {
         InputOption::VALUE_OPTIONAL,
         'Specify which branch to build if different than the global branch found in sites.yml'
       );
+    //@todo populate other commands artuments here.
   }
 
   /**
@@ -68,25 +71,24 @@ class BuildCommand extends AbstractCommand {
     parent::execute($input, $output);
 
     $commands = array(
-      sprintf('drupal site:checkout %s', $this->siteName),
-      $this->getComposerMakeCommand(),
-      'drupal site:npm %s',
-      'drupal site:grunt %s',
-      'drupal site:settings %s',
-      'drupal site:phpunit:setup %s',
-      'drupal site:test:setup %s',
-      'drupal site:db:import %s',
-      'drupal site:update %s',
+      'site:checkout' => sprintf('%s', $this->siteName),
+      $this->getComposerMakeCommand() => sprintf('%s', $this->siteName),
+      'site:npm' => sprintf('%s', $this->siteName),
+      'site:grunt' => sprintf('%s', $this->siteName),
+      'site:settings' => sprintf('%s', $this->siteName),
+      'site:phpunit:setup' => sprintf('%s', $this->siteName),
+      'site:test:setup' => sprintf('%s', $this->siteName),
+      'site:db:import' => sprintf('%s', $this->siteName),
+      'site:update' => sprintf('%s', $this->siteName),
     );
 
-    foreach ($commands as $commandName) {
+    foreach ($commands as $commandName => $args) {
       $command = $this->getApplication()->find($commandName);
 
       $parameters = $input->getArguments();
       foreach ($input->getOptions() as $name => $value) {
         $parameters['--' . $name] = $value;
       }
-      print_r($parameters);
       $commandInput = new ArrayInput(array_filter($parameters));
 
       $command->run($commandInput, $output);
@@ -97,20 +99,13 @@ class BuildCommand extends AbstractCommand {
    * Detect if the site uses composer or make files.
    */
   private function getComposerMakeCommand() {
-   if (file_exists($this->getWebRoot() . '/compsoser.json')) {
-      $composerMakeCommand = 'drupal site:compose %s';
+   if (file_exists($this->getRoot() . '/composer.json')) {
+      return 'site:compose';
     }
-    else if (file_exists($this->getWebRoot() . '/site.make')) {
-      $composerMakeCommand = 'drupal site:compose %s';
+    else if (file_exists($this->getRoot() . '/site.make')) {
+      return 'site:make';
     }
-    else if (file_exists($this->getWebRoot() . sprintf('/%s.make', $this->siteName))) {
-      $composerMakeCommand = 'drupal site:compose %s';
-    }
-    else {
-      $composerMakeCommand = '';
-    }
-
-    return $composerMakeCommand;
+    throw new CommandException(sprintf('Could not find composer.json or site.make in %s', $this->getRoot()));
   }
 
 }
