0366ab5f1bd418e42dd3086101e43a2179b25ab7
Marcelo <marcellovani@yahoo.co.uk>
2017-08-23 15:39:14 +0100

    Update install_centos6.sh

--- scripts/install_centos6.sh
+++ scripts/install_centos6.sh
@@ -23,7 +23,7 @@ sh install_launcher.sh
 # In order to use the custom php on centos6 we need to add a wrapper for drupal
 sudo mv /usr/local/bin/drupal /usr/local/drupal.phar
 echo '#!/bin/sh' > /tmp/drupal
-echo 'PATH="${PHP_FOLDER}/:$PATH" /usr/local/drupal.phar "$@"' >> /tmp/drupal
+echo 'PATH="/opt/php/bin/:$PATH" /usr/local/drupal.phar "$@"' >> /tmp/drupal
 sudo mv /tmp/drupal /usr/local/bin/drupal
 sudo chmod +x /usr/local/bin/drupal
 
