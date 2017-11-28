83969f8567504578ee516eda7a0d1452b71fcb19
Peter Clarke <peter_clarke@dennis.co.uk>
2017-11-28 17:38:02 +0000

    Pin to launcher 1.0.2

--- scripts/install_launcher.sh
+++ scripts/install_launcher.sh
@@ -9,6 +9,6 @@ if [ -e "${DRUPAL_CONSOLE}" ]; then
   sudo rm ${DRUPAL_CONSOLE}
 fi
 
-PATH="${PHP_FOLDER}/:$PATH" php -r "readfile('https://drupalconsole.com/installer');" > ~/drupal.phar
+PATH="${PHP_FOLDER}/:$PATH" php -r "readfile('https://github.com/hechoendrupal/drupal-console-launcher/releases/download/1.0.2/drupal.phar');" > ~/drupal.phar
 sudo chmod +x ~/drupal.phar
 sudo mv ~/drupal.phar ${DRUPAL_CONSOLE}
