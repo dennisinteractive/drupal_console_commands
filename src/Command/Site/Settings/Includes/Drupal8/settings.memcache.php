be5e13a351f67cfcf95c5256f12011d22f4e0f13
Simon Peacock <simon_peacock@dennis.co.uk>
2018-10-13 09:24:55 +0100

    containerised_builds - chaning memecache server host for containerised builds

--- src/Command/Site/Settings/Includes/Drupal8/settings.memcache.php
+++ src/Command/Site/Settings/Includes/Drupal8/settings.memcache.php
@@ -2,7 +2,7 @@
 /**
  * Memcache settings.
  */
-$settings['memcache']['servers'] = ['127.0.0.1:11211' => 'default'];
+$settings['memcache']['servers'] = ['dennis-memcached:11211' => 'default'];
 $settings['memcache']['bins'] = ['default' => 'default'];
 $settings['memcache']['key_prefix'] = '${memcache_prefix}';
 $settings['memcache']['stampede_protection'] = TRUE;
