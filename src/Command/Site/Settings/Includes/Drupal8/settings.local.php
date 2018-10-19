5f5d895e658ed950c9ae0d04f2d4be0680bf4eec
simon-peacock <simon_peacock@dennis.co.uk>
2018-10-19 12:16:43 +0100

    Update settings.local.php

--- src/Command/Site/Settings/Includes/Drupal8/settings.local.php
+++ src/Command/Site/Settings/Includes/Drupal8/settings.local.php
@@ -1,7 +1,7 @@
 <?php
 // Set Stage file proxy origin.
 //$config['stage_file_proxy.settings']['origin'] = '${cdn}';
-$config['stage_file_proxy.settings']['origin'] = 'cdn.drivingelectric.com'
+$config['stage_file_proxy.settings']['origin'] = 'cdn.drivingelectric.com';
 
 // Change CDN domain to local.
 $config['cdn.settings']['mapping']['domain'] = '${host}';
